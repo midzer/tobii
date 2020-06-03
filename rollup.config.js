35e55fe5a99c8c4d159a8a00505969782e491622
Benjamin <rqrauhvmra@users.noreply.github.com>
2020-06-03 21:08:06 +0200

    Edit config

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,130 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import babel from 'rollup-plugin-babel'
-import minify from 'rollup-plugin-babel-minify'
+import { eslint } from 'rollup-plugin-eslint'
+import { terser } from 'rollup-plugin-terser'
+import postcss from 'rollup-plugin-postcss'
+import babel from '@rollup/plugin-babel'
 
-export default [
-  {
-    input: './src/js/tobii.js',
-    output: [
-      {
-        name: 'Tobii',
-        file: './dist/js/tobii.js',
-        format: 'umd'
-      },
-      {
-        file: './dist/js/tobii.cjs.js',
-        format: 'cjs'
-      }
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: 'node_modules/**',
-        presets: [
-          ['@babel/env', {
-            corejs: 3.6,
-            useBuiltIns: 'usage',
-            loose: true
-          }]
-        ]
-      })
-    ]
-  },
-  // Minfied browser-friendly UMD build
-  {
-    input: './src/js/tobii.js',
-    output: {
+import license from 'rollup-plugin-license'
+import filesize from 'rollup-plugin-filesize'
+import cssnano from 'cssnano'
+import autoprefixer from 'autoprefixer'
+import stylelint from 'rollup-plugin-stylelint'
+
+const pkg = require('./package.json')
+const banner = ['/**\n *', pkg.name, pkg.version, '\n *\n */'].join(' ')
+
+export default [{
+  input: './src/js/tobii.js',
+  output: [
+    {
+      sourcemap: false,
       name: 'Tobii',
-      file: './dist/js/tobii.min.js',
+      file: './dist/js/tobii.js',
       format: 'umd'
     },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: 'node_modules/**',
-        presets: [
-          ['@babel/env', {
-            corejs: 3.6,
-            useBuiltIns: 'usage',
-            loose: true
-          }]
-        ]
-      }),
-      minify({
-        comments: false
-      })
-    ]
+    {
+      sourcemap: false,
+      name: 'Tobii',
+      file: './dist/js/tobii.cjs.js',
+      format: 'cjs'
+    }
+  ],
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    eslint({
+      include: [
+        './src/js/tobii.js'
+      ]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        ['@babel/preset-env', {
+          corejs: 3.6,
+          useBuiltIns: 'usage'
+        }]
+      ]
+    }),
+    stylelint({
+      fix: false,
+      include: [
+        './src/scss/**.scss'
+      ],
+      syntax: 'scss',
+      quiet: false
+    }),
+    postcss({
+      plugins: [
+        autoprefixer
+      ],
+      extract: '@/../../css/tobii.css'
+    }),
+    license({
+      banner
+    }),
+    filesize({
+      showMinifiedSize: false
+    })
+  ],
+  watch: {
+    clearScreen: false
   }
-]
+},
+{
+  input: './src/js/tobii.js',
+  output: {
+    sourcemap: false,
+    name: 'Tobii',
+    file: './dist/js/tobii.min.js',
+    format: 'umd'
+  },
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    eslint({
+      include: [
+        './src/js/tobii.js'
+      ]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        ['@babel/preset-env', {
+          corejs: 3,
+          useBuiltIns: 'usage'
+        }]
+      ]
+    }),
+    stylelint({
+      fix: false,
+      include: [
+        './src/scss/**.scss'
+      ],
+      syntax: 'scss',
+      quiet: false
+    }),
+    postcss({
+      plugins: [
+        autoprefixer,
+        cssnano
+      ],
+      extract: '@/../../css/tobii.min.css'
+    }),
+    terser(),
+    license({
+      banner
+    }),
+    filesize({
+      showMinifiedSize: false
+    })
+  ],
+  watch: {
+    clearScreen: false
+  }
+}]
