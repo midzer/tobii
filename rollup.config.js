b8042be84df8250bcf7e2f500d4598bba738c905
Benjamin <rqrauhvmra@users.noreply.github.com>
2020-07-09 10:19:33 +0200

    Small adjustments

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,8 @@ import { eslint } from 'rollup-plugin-eslint'
 import { terser } from 'rollup-plugin-terser'
 import postcss from 'rollup-plugin-postcss'
 import babel from '@rollup/plugin-babel'
-
 import license from 'rollup-plugin-license'
-import filesize from 'rollup-plugin-filesize'
 import cssnano from 'cssnano'
-import autoprefixer from 'autoprefixer'
-import stylelint from 'rollup-plugin-stylelint'
 
 const pkg = require('./package.json')
 const banner = ['/**\n *', pkg.name, pkg.version, '\n *\n */'].join(' ')
@@ -41,6 +37,7 @@ export default [{
       ]
     }),
     babel({
+      babelHelpers: 'bundled',
       exclude: 'node_modules/**',
       presets: [
         ['@babel/preset-env', {
@@ -49,25 +46,14 @@ export default [{
         }]
       ]
     }),
-    stylelint({
-      fix: false,
-      include: [
-        './src/scss/**.scss'
-      ],
-      syntax: 'scss',
-      quiet: false
-    }),
     postcss({
       plugins: [
-        autoprefixer
+        require('postcss-preset-env')
       ],
       extract: '@/../../css/tobii.css'
     }),
     license({
       banner
-    }),
-    filesize({
-      showMinifiedSize: false
     })
   ],
   watch: {
@@ -93,6 +79,7 @@ export default [{
       ]
     }),
     babel({
+      babelHelpers: 'bundled',
       exclude: 'node_modules/**',
       presets: [
         ['@babel/preset-env', {
@@ -101,17 +88,9 @@ export default [{
         }]
       ]
     }),
-    stylelint({
-      fix: false,
-      include: [
-        './src/scss/**.scss'
-      ],
-      syntax: 'scss',
-      quiet: false
-    }),
     postcss({
       plugins: [
-        autoprefixer,
+        require('postcss-preset-env'),
         cssnano
       ],
       extract: '@/../../css/tobii.min.css'
@@ -119,9 +98,6 @@ export default [{
     terser(),
     license({
       banner
-    }),
-    filesize({
-      showMinifiedSize: false
     })
   ],
   watch: {
