06448ec2572b2210975066fee1e08e535420814a
midzer <midzer@gmail.com>
2022-06-07 13:32:53 +0200

    fix eslint

--- src/js/polyfill/forEach.js
+++ src/js/polyfill/forEach.js
@@ -4,10 +4,10 @@
  *
  */
 if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, thisArg) {
-        thisArg = thisArg || window;
-        for (var i = 0; i < this.length; i++) {
-            callback.call(thisArg, this[i], i, this);
-        }
-    };
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this)
+    }
+  }
 }
