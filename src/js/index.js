aa3f6494c907829af3cb91f045ae9b190838debb
midzer <midzer@gmail.com>
2025-08-11 22:58:23 +0200

    zoom functionality only for zoomable elements

--- src/js/index.js
+++ src/js/index.js
@@ -1001,6 +1001,7 @@ export default function Tobii (userOptions) {
       ) / DRAG.distance
 
       zoomPan(
+        event.target,
         clamp(scale, MIN_SCALE, MAX_SCALE),
         x, y,
         x - DRAG.x, y - DRAG.y
@@ -1089,7 +1090,7 @@ export default function Tobii (userOptions) {
         if (isZoomed()) {
           resetZoom()
         } else {
-          zoomPan(MAX_SCALE / 2, x, y, 0, 0)
+          zoomPan(event.target, MAX_SCALE / 2, x, y, 0, 0)
         }
       } else {
         lastTapTime = now
@@ -1114,12 +1115,17 @@ export default function Tobii (userOptions) {
    *
    */
   const wheelHandler = (event) => {
+    if (!isZoomableElement(event.target)) {
+      return
+    }
+
     const deltaScale = Math.sign(event.deltaY) > 0 ? -1 : 1
     if (!isZoomed() && !deltaScale) return
     event.preventDefault()
 
     const newScale = TRANSFORM.scale + deltaScale / (SCALE_SENSITIVITY / TRANSFORM.scale)
     zoomPan(
+      event.target,
       clamp(newScale, MIN_SCALE, MAX_SCALE),
       event.clientX, event.clientY,
       0, 0
@@ -1168,16 +1174,16 @@ export default function Tobii (userOptions) {
     element.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`
   }
 
-  const zoomPan = (newScale, x, y, deltaX, deltaY) => {
-    if (!TRANSFORM.element) {
-      TRANSFORM.element = lightbox.querySelector('.tobii__slide--is-active img')
-    }
-    const { left, top } = TRANSFORM.element.getBoundingClientRect()
+  const zoomPan = (el, newScale, x, y, deltaX, deltaY) => {
+    if (!isZoomableElement(el)) return
+
+    const { left, top } = el.getBoundingClientRect()
     const originX = x - left
     const originY = y - top
     const newOriginX = originX / TRANSFORM.scale
     const newOriginY = originY / TRANSFORM.scale
 
+    TRANSFORM.element = el
     TRANSFORM.originX = newOriginX
     TRANSFORM.originY = newOriginY
     TRANSFORM.scale = newScale
@@ -1361,12 +1367,20 @@ export default function Tobii (userOptions) {
   }
 
   /**
-   * Checks whether element's nodeName is part of array
+   * Checks whether element's tagName is part of array
    *
    */
   const isIgnoreElement = (el) => {
-    return ['TEXTAREA', 'OPTION', 'INPUT', 'SELECT'].indexOf(el.nodeName) !== -1 || el === prevButton ||
+    return ['TEXTAREA', 'OPTION', 'INPUT', 'SELECT'].indexOf(el.tagName) !== -1 || el === prevButton ||
       el === nextButton || el === closeButton
+  }
+
+  /**
+   * Checks whether an element is zoomable
+   *
+   */
+  const isZoomableElement = (el) => {
+    return el.tagName === 'IMG'
   }
 
   /**
